37b3076018b1a4b8056444db2d684ab9d93a18cb
Yuan Gao <alssica@gmail.com>
2019-08-11 21:47:50 -0400

    test

--- proposal/project/app.py
+++ proposal/project/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
-from app import app
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
